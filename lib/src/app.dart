145e7e9716f5ff3208eefcaee5340ce3906ebbcd
TALABI AYOMIDE <flioolawale@gmail.com>
2024-01-08 12:12:33 +0100

    features(upgrade): Installed GoRouter package for easy navigation from Navigator 1.0, Initialized and configured a router for navigating across the application

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,5 +1,5 @@
-import 'package:ecommerce_app/src/features/products_list/products_list_screen.dart';
 import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
+import 'package:ecommerce_app/src/routes/route.dart';
 import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
@@ -7,11 +7,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    final router = AppRouter.initializeRouter();
+    return MaterialApp.router(
+      // * The routing configuration of the app
+      routerDelegate: router.routerDelegate,
+      routeInformationParser: router.routeInformationParser,
+      routeInformationProvider: router.routeInformationProvider,
+      // * Every other configuration
       debugShowCheckedModeBanner: false,
       restorationScopeId: 'app',
-      // * The home page of the app
-      home: const ProductsListScreen(),
       onGenerateTitle: (BuildContext context) => 'My Shop'.hardcoded,
       theme: ThemeData(
         primarySwatch: Colors.grey,
