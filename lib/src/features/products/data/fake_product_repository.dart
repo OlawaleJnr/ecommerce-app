e38c6fd69de350dfb93c77bed7ba96bb1b33fe50
TALABI AYOMIDE <flioolawale@gmail.com>
2024-01-25 19:35:50 +0100

    features(upgrade): Implemented futures and stream based API in FakeProductRepository

--- lib/src/features/products/data/fake_product_repository.dart
+++ lib/src/features/products/data/fake_product_repository.dart
@@ -3,33 +3,29 @@ import 'package:ecommerce_app/src/features/products/domain/product.dart';
 
 class FakeProductRepository {
 
-  /// `FakeProductRepository._();` is a private constructor for the `FakeProductRepository` class. By
-  /// using the underscore `_`, the constructor is made private and can only be accessed within the
-  /// class itself. This means that the `FakeProductRepository` class cannot be instantiated from
-  /// outside the class.
   FakeProductRepository._();
 
-  /// The line `static FakeProductRepository instance = FakeProductRepository._();` is creating a static
-  /// instance of the `FakeProductRepository` class.
   static FakeProductRepository instance = FakeProductRepository._();
 
-  /// The function `getProductList` returns a list of products.
-  /// 
-  /// Returns:
-  ///   The method is returning a list of Product objects.
+  final List<Product> _products = kTestProducts;
+
   List<Product> getProductList () {
-    return kTestProducts; 
+    return _products; 
   }
 
-  /// The function `getProduct` returns a `Product` object with a matching `id` from a list of
-  /// `kTestProducts`.
-  /// 
-  /// Args:
-  ///   id (String): The id parameter is a string that represents the unique identifier of a product.
-  /// 
-  /// Returns:
-  ///   The getProduct function returns a Product object.
   Product? getProduct(String id) {
-    return kTestProducts.firstWhere((product) => product.id == id);
+    return _products.firstWhere((product) => product.id == id);
+  }
+
+  Future<List<Product>> fetchProductList() {
+    return Future.value(_products);
+  }
+
+  Stream<List<Product>> watchProductList() {
+    return Stream.value(_products);
+  }
+
+  Stream<Product?> watchProduct(String id) {
+    return watchProductList().map((products) => products.firstWhere((product) => product.id == id));
   }
 } 
